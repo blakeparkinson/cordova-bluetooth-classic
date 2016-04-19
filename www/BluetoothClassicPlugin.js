4c1496c8a5d842471904826e4dae6d10dc8b2535
blakeparkinson <wizardplow@gmail.com>
2016-04-19 17:53:12 -0600

    updating the xml

--- www/BluetoothClassicPlugin.js
+++ www/BluetoothClassicPlugin.js
@@ -3,6 +3,7 @@
 module.exports = {
 
     connect: function (id, success, failure) {
+      console.log('made it');
       cordova.exec(success, failure, 'BluetoothClassic', 'connect', [id]);
     }
 };
